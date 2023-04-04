659a792a54f56ba3e7fab63b623a589741e4a079
jarkendia <harukendia@gmail.com>
2023-04-04 11:17:49 +0200

    Finalizados todos los cambios y añadidos

--- js/main.js
+++ js/main.js
@@ -2,12 +2,10 @@
 
 const sounds = document.querySelectorAll(".durums");
 
-// Botón de grabar
+// Botón de grabar, stop y play
 const recorderBtn = document.querySelector("#recorder");
 const stopBtn = document.querySelector("#stop");
 const playBtn = document.querySelector("#play");
-
-// const recordedMusic = {};
 
 const soundPlay = (click) => {
   const idKey = click.target.id;
@@ -18,54 +16,14 @@ for (const sound of sounds) {
   sound.addEventListener("click", soundPlay);
 }
 
+// Creamos un array vacio para guardar los botones de la batería presionados.
 const grabaciones = [];
+// grabacionActiva: daría como valor false para que solo grabe cuando su valor sea true.
 let grabacionActiva = false;
+// tiempoAnterior: esta variable almacena el tiempo en que se presiono el botón anterior en milisegundos.
 let tiempoAnterior = null;
-
-const grabacionesSelect = document.querySelector("#grabacionesSelect");
-
-// Agrega las opciones de grabación al elemento "select"
-for (let i = 0; i < localStorage.length; i++) {
-  const clave = localStorage.key(i);
-  if (clave.includes("-")) {
-    const opcion = document.createElement("option");
-    opcion.text = clave;
-    opcion.value = clave;
-    grabacionesSelect.add(opcion);
-  }
-}
-
-function leerGrabacion(claveGrabacion) {
-  const grabacionSerializada = localStorage.getItem(claveGrabacion);
-  const grabacion = JSON.parse(grabacionSerializada);
-  grabaciones.length = 0; // Vaciar el array actual
-  grabaciones.push(...grabacion); // Agregar los nuevos datos
-}
-
-// Agrega un nuevo evento para cargar las grabaciones seleccionadas del Local Storage
-grabacionesSelect.addEventListener("change", () => {
-  const claveGrabacion = grabacionesSelect.value;
-  grabaciones.length = 0;
-  leerGrabacion(claveGrabacion);
-});
-
-const formulario = document.querySelector("form");
-
-formulario.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const nombreGrabacion = document.querySelector("#nombreGrabacion").value;
-  const etiquetaGrabacion = document.querySelector("#etiquetaGrabacion").value;
-  const claveGrabacion = `${nombreGrabacion}-${etiquetaGrabacion}`;
-  const grabacionSerializada = JSON.stringify(grabaciones);
-  localStorage.setItem(claveGrabacion, grabacionSerializada);
-  alert(
-    `La grabación "${nombreGrabacion}" ha sido guardada con la etiqueta "${etiquetaGrabacion}".`
-  );
-
-  grabaciones.length = 0;
-  tiempoAnterior = null;
-});
-
+// Recorre el array de sounds y agrega a cada botón(en este caso serían los botones de la batería) un "EventListener" para que cada vez que se haga click en el botón si "grabacionActiva" es true crea una constante con el tiempo actual, otra constante con el soundName(el nombre del sónido que hemos asignado nosotros en el HTML) y otra con el "tiempoTranscurrido" entre los clicks de los botones. Por último crea la constante "grabacion" con un objeto que contiene las 3 constantes anteriores dentro de sus respectivas propiedades.
+// Por úlimo hace un push y guarda "grabacion" dentro de "grabaciones". También al final del bucle "tiempoAnterior" guarda el valor de "tiempoActual" para asi guardar el "tiempoAnterior" en el que se hizo click el botón.
 sounds.forEach((boton) => {
   boton.addEventListener("click", () => {
     if (grabacionActiva) {
@@ -84,16 +42,18 @@ sounds.forEach((boton) => {
     }
   });
 });
-
+// El botón de grabacion.
 recorderBtn.addEventListener("click", () => {
   grabacionActiva = true;
   tiempoAnterior = null;
 });
-
+// El botón de stop.
 stopBtn.addEventListener("click", () => {
   grabacionActiva = false;
 });
-
+// El botón de play: Recorremos cada objeto del array "grabaciones" creamos una constante "tiempoTranscurrido" que accede al valor de la propiedad "elapsedTime" o 0 si no existiera el valor de "elapsedTime". Luego el setTimeout reproduce el "new Audio" y dependiendo del botón que se pulso al hacer la grabacion, la propiedad ".name" de cada "grabacion" se añade a la string de "new Audio", de esa forma siempre reproduce el sonido correspondiente.
+// Por último el setTimeout reproduce cada sónido según el tiempo que le indica "tiempoTranscurridoTotal".
+// Al final de cada vuelta del bucle "tiempoTranscurridoTotal" pasa a tener el valor de "tiempoTranscurrido", de esta manera la primera vez se reproduce en 0 milisegundos(que es el valor que tiene) y la siguiente se reproduce con el tiempo exacto en el que se guardo al hacer la grabación, así sucesivamente.
 playBtn.addEventListener("click", () => {
   let tiempoTranscurridoTotal = 0;
   grabaciones.forEach((grabacion) => {
@@ -108,7 +68,59 @@ playBtn.addEventListener("click", () => {
   });
 });
 
+const formulario = document.querySelector("form");
+
+formulario.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const nombreGrabacion = document.querySelector("#nombreGrabacion").value;
+  const etiquetaGrabacion = document.querySelector("#etiquetaGrabacion").value;
+  const claveGrabacion = `${nombreGrabacion}-${etiquetaGrabacion}`;
+  const grabacionSerializada = JSON.stringify(grabaciones);
+  localStorage.setItem(claveGrabacion, grabacionSerializada);
+  const opcion = document.createElement("option");
+  opcion.text = claveGrabacion;
+  opcion.value = claveGrabacion;
+  grabacionesSelect.add(opcion);
+  alert(
+    `La grabación "${nombreGrabacion}" ha sido guardada con la etiqueta "${etiquetaGrabacion}".`
+  );
+
+  grabaciones.length = 0;
+  tiempoAnterior = null;
+});
+
+const grabacionesSelect = document.querySelector("#grabacionesSelect");
+
+const leerGrabacion = (claveGrabacion) => {
+  const grabacionSerializada = localStorage.getItem(claveGrabacion);
+  const grabacion = JSON.parse(grabacionSerializada);
+  grabaciones.length = 0; // Vaciar el array actual
+  grabaciones.push(...grabacion); // Agregar los nuevos datos
+};
+
+// Agrega un nuevo evento para cargar las grabaciones seleccionadas del Local Storage
+grabacionesSelect.addEventListener("change", () => {
+  const claveGrabacion = grabacionesSelect.value;
+  grabaciones.length = 0;
+  leerGrabacion(claveGrabacion);
+});
+
+// Agrega las opciones de grabación al elemento "select"
+for (let i = 0; i < localStorage.length; i++) {
+  const clave = localStorage.key(i);
+  console.log(localStorage.key(i));
+  if (clave.includes("-")) {
+    const opcion = document.createElement("option");
+    opcion.text = clave;
+    opcion.value = clave;
+    grabacionesSelect.add(opcion);
+  }
+}
+
 const soundPlayKey = (event) => {
+  if (event.target.tagName === "INPUT") {
+    return;
+  }
   switch (event.key.toLowerCase()) {
     case "a":
       crash.click();
@@ -224,3 +236,17 @@ const fartNoise = new Audio("sounds/fart.wav");
 fartBtn.addEventListener("click", (event) => {
   fartNoise.play();
 });
+
+////////////////////
+// Documentación //
+//////////////////
+// Consultar INFO https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement
+// En concreto https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement
+// Código de teclas https://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
+// Ayuda teclas https://stackoverflow.com/questions/12578379/play-a-sound-when-a-key-is-pressed
+// Cómo grabar https://medium.com/@bryanjenningz/how-to-record-and-play-audio-in-javascript-faa1b2b3e49b
+// Otro ejemplo https://ralzohairi.medium.com/audio-recording-in-javascript-96eed45b75ee
+// Saber duración https://es.stackoverflow.com/questions/408666/obtener-la-duracion-de-un-audio-con-javascript
+// MDN sobre EVENTOS https://developer.mozilla.org/en-US/docs/Web/Events
+// Ideas: cursor ratón https://www.cristalab.com/tutoriales/como-personalizar-el-cursor-con-javascript-c45943l/
+// https://www.w3schools.com/jsref/prop_style_cursor.asp
